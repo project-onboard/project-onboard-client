c2219ff1208bb548b1b64b54bd68014da4ddd974
Paige Sun <psun@tribalscale.com>
2019-03-20 14:09:12 -0400

    Update module and section headers

--- onboard-react-client/src/App.jsx
+++ onboard-react-client/src/App.jsx
@@ -1,12 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 import Simple from './Simple'
+import { FontIcon } from 'react-md';
+
+// import inboxListItems from './react-md-src/constants/inboxListItems';
 
 class App extends Component {
   render() {
+    const modules = [
+      {
+        title: "Intro to slack",
+        contents: [
+          {
+            type: "text",
+            title: "Slack 101"
+          },
+          {
+            type: "text",
+            title: "Hello world"
+          }
+        ]
+      },
+      {
+        title: "Interacting with people",
+        contents: [
+          {
+            type: "text",
+            title: "Learn to chat"
+          },
+          {
+            type: "text",
+            title: "Learn to add emojis"
+          },
+          {
+            type: "text",
+            title: "Learn to add reactions"
+          },
+          {
+            type: "text",
+            title: "Learn to make polls"
+          }
+        ]
+      }
+    ];
+
+    const inboxListItems = modules.map((courseModule, moduleIndex) => {
+      const moduleHeader = {
+      key: 'header' + moduleIndex,
+      primaryText: courseModule.title,
+      leftIcon: <FontIcon>school</FontIcon>,
+      active: false,
+    }
+  
+      const string =  '\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0'
+      const sections = courseModule.contents.map((section, sectionIndex) => {
+        return  {
+          key: 'section' + moduleIndex + 'contentItems' + sectionIndex,
+          primaryText: string+ section.title,
+          // leftIcon: <FontIcon>inbox</FontIcon>,
+          active: false,
+        }
+      });
+  
+      return [moduleHeader, ...sections]
+    });
+  
+
+    console.log("inbox list items ", inboxListItems.flat())
+
     return (
         <div className="App">
-          <Simple />
+          { <Simple inboxListItems={inboxListItems.flat()}/> }
         </div>
     );
   }
--- onboard-react-client/src/Simple.jsx
+++ onboard-react-client/src/Simple.jsx
@@ -3,16 +3,15 @@ import React, { PureComponent } from 'react';
 import { Button, NavigationDrawer, SVGIcon } from 'react-md';
 import { ReactComponent as MenuIcon } from './react-md-src/icons/menu.svg';
 import arrowBack from './react-md-src/icons/arrow_back.svg';
-import inboxListItems from './react-md-src/constants/inboxListItems';
 import { FontIcon } from 'react-md';
 
 const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 export default class Simple extends PureComponent {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     // Update the items so they have an onClick handler to change the current page
-    this.navItems = inboxListItems.map((item) => {
+    this.navItems = props.inboxListItems.map((item) => {
       if (item.divider) {
         return item;
       }
@@ -26,8 +25,8 @@ export default class Simple extends PureComponent {
     this.state = {
       renderNode: null,
       visible: false,
-      key: inboxListItems[0].key,
-      page: inboxListItems[0].primaryText,
+      key: props.inboxListItems[0].key,
+      page: props.inboxListItems[0].primaryText,
     };
   }
 
