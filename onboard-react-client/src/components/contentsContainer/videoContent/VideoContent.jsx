dfd9acbe2898d419b401f46f1d7727c934fd13e0
jgoping <jgoping@gmail.com>
2019-03-21 22:32:10 -0400

    Add ability to display videos

---
+++ onboard-react-client/src/components/contentsContainer/videoContent/VideoContent.jsx
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+import { TextField } from 'react-md';
+import './videocontent.css'
+
+export default class VideoContent extends PureComponent {
+  state = {
+    url: 'jz10GvbC8bA'
+  };
+
+  handleUrlChange = url => {
+    this.setState({ url });
+  };
+
+  render() {
+    if (this.props.isEditing) {
+      return (
+        <div className='video-content-box'>
+            <div className='youtube-text'>https://www.youtube.com/watch?v=</div>
+            <div className='spacing-after-youtube-text'></div>
+          <TextField
+            id='url'
+            placeholder='Enter a URL to a video here'
+            onChange={this.handleUrlChange}
+            value={this.state.url}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <iframe
+          width='800'
+          height='450'
+          frameborder='0'
+          allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+          src={"https://www.youtube.com/embed/" + this.state.url}
+        />
+      );
+    }
+  }
+}
